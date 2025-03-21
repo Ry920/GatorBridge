5103ac0629a7eade595c101db3fb814346b32273
Ry920 <rymbrito@gmail.com>
2025-03-21 18:58:04 -0400

    Starting to write test cases for frontend components.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Home from './pages/home';
 
-test('renders learn react link', () => {
+test('renders Log in and Sign up', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Log in/i);
+  const linkElement2 = screen.getByText(/Sign up/i);
+  expect(linkElement).toBeInTheDocument()
+  expect(linkElement2).toBeInTheDocument();
+});
+
+test('renders Profile link', () => {
+  render(<Home />);
+  const linkElement = screen.getByText(/Profile/i);
   expect(linkElement).toBeInTheDocument();
 });
