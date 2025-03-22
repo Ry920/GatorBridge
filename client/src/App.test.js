a952162e09eaef895f450246e6b1fd68a1e1eba5
Ry920 <rymbrito@gmail.com>
2025-03-22 14:22:44 -0400

    Added login and signup tests, will fail until signup and login functionality is finalized.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router";
 import React from "react";
 import userEvent from "@testing-library/user-event"
 
-test('renders Log in and Sign up', () => {
+test('Renders Log in and Sign up', () => {
   render(<App />);
   const linkElement = screen.getAllByText(/Log in/i)[0];
   const linkElement2 = screen.getAllByText(/Sign up/i)[0];
@@ -14,7 +14,7 @@ test('renders Log in and Sign up', () => {
   expect(linkElement2).toBeInTheDocument();
 });
 
-test('renders Profile link', async () => {
+test('Renders Profile link', async () => {
   render(<Router>
     <Home />
   </Router>);
@@ -24,8 +24,62 @@ test('renders Profile link', async () => {
   expect(screen.getAllByText(/Profile/i)[0]).toBeInTheDocument();
 });
 
-test('renders Edit Profile', () => {
+test('Renders Edit Profile', () => {
   render(<Profile />);
   const linkElement = screen.getByText(/Edit Profile/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+describe('Sign up and Log in Tests', () => {
+  let mockedFn;
+  beforeEach(() => {
+    mockedFn = jest.fn(Home);
+  });
+  afterEach(() => {
+    mockedFn.mockRestore();
+  });
+  test('Sign up functionality', () => {
+    mockedFn.mockImplementation(() => <h1>This is the Home Page test</h1>);
+    const {container} = render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>);
+    const firstName = container.getElementsByClassName("App-FirstName-TextField")[0];
+    const lastName = container.getElementsByClassName("App-LastName-TextField")[0];
+    const email = container.getElementsByClassName("App-GetAddress-TextField")[0];
+    const password = container.getElementsByClassName("App-GetPassword-TextField")[0];
+    const signupButton = container.getElementsByClassName("App-SignUp-Button-Container")[0];
+  
+    expect(firstName).toBeInTheDocument();
+    expect(lastName).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(signupButton).toBeInTheDocument();
+    
+    userEvent.type(firstName, 'John');
+    userEvent.type(lastName, 'Doe');
+    userEvent.type(email, 'jdoetesting@ufl.edu');
+    userEvent.type(password, 'testing12345');
+    userEvent.click(signupButton);
+    expect(screen.getByText("This is the Home Page test")).toBeInTheDocument();
+  });
+  test('Log in functionality', () => {
+    mockedFn.mockImplementation(() => <h1>This is the Home Page test</h1>);
+    const {container} = render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>);
+    const email = container.getElementsByClassName("App-GetAddress-TextField")[0];
+    const password = container.getElementsByClassName("App-GetPassword-TextField")[0];
+    const loginButton = container.getElementsByClassName("App-LogIn-Button-Container")[0];
+
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+    userEvent.type(email, 'jdoetesting@ufl.edu');
+    userEvent.type(password, 'testing12345');
+    userEvent.click(loginButton);
+    expect(screen.getByText("This is the Home Page test")).toBeInTheDocument();
+  });
 });
