ec36ab2bccc19759affc0dd90f287b3da8ab2fcf
Ryan Brito <rymbrito@gmail.com>
2025-03-24 01:18:16 -0400

    started work on home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,20 +4,22 @@ import "./home.css";
 import { Link } from "react-router";
 
 function Home() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
+  const [postText, setPostText] = React.useState("");
+  const [textSize, setTextSize] = React.useState(255);
+  const maxSize = 255;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (textSize < 0) return;
+  }
   return (
     <div className="Home">
       <header className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-        <Link to="/profile">Profile</Link>
+        <form>
+          <text>What do you want to discuss?</text>
+          <input type="text" onChange={(e) => {setPostText(e.target.value); setTextSize(255 - e.target.value.length)}}></input>
+          <text>{textSize}</text>
+          <button type="submit">Post</button>
+        </form>
       </header>
     </div>
   );
