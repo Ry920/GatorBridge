ba2b8262496b7a86fefeac4c604867bd46769809
w-acostacruz <wacosta3456@gmail.com>
2025-04-15 13:59:07 -0400

    username and bio sync with backend; username can be updated using edit button; added "username" and "biography" columns to backend; code should check for nulls and add defaults accordingly, if username is null, it will use email since they are distinct. Need to update css for edit profile as it curr relies on size of username, instead want it to be fixed in place.

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,11 +1,82 @@
-import React from "react"
+import React, { useEffect } from "react"
 import "./profile.css"
 import pfp from "../R.jpg"
 import Popup from "./editPopup.js"
+import { Link } from "react-router";
 
 function Profile(){
+    const [username, setUsername] = React.useState("");
     const [toEdit, settoEdit] = React.useState(false);
     const [onHover, setOnHover] = React.useState(false);
+    const [descriptionText, setDescriptionText] = React.useState("");
+    const handleDescriptionFetch = async (event) => {
+        // event.preventDefault();
+        const requestOptions = {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${localStorage.getItem('token')}` },
+        };
+        const response = await fetch("/biography", requestOptions);
+        console.log("Response status:", response.status);
+        if (response.ok) {
+            const desc = await response.json();
+            console.log(desc);
+            setDescriptionText(desc[0].biography);
+        }
+        else {
+            alert("Error fetching biography");
+        }
+        }
+    const handleUserSetDef = async (event) =>{
+        const requestOptions = {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${localStorage.getItem('token')}` },
+        };
+        const response = await fetch("/userSetDef", requestOptions);
+        console.log("Response status:", response.status);
+        if (response.ok) {
+            await handleUsernameFetch();
+        }
+        else {
+            alert("Error setting default username");
+        }
+    }    
+    const handleUsernameCheck = async (event) => {
+        const requestOptions = {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${localStorage.getItem('token')}` },
+        };
+        const response = await fetch("/usernameCheckDefault", requestOptions);
+        console.log("Response status:", response.status);
+        if (response.ok) {
+            const data = await response.json();
+            if (data.userNull){
+                await handleUserSetDef();
+            }
+            else{
+                await handleUsernameFetch();
+            }
+        }
+        else {
+            alert("Error changing usernameCheck");
+        }
+    }    
+    const handleUsernameFetch = async (event) => {
+        // event.preventDefault();
+        const requestOptions = {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+        };
+        const response = await fetch("/username", requestOptions);
+        console.log("Response status:", response.status);
+        if (response.ok) {
+            const user = await response.json();
+            console.log(user[0].username);
+            setUsername(user[0].username);
+        }
+        else {
+            alert("Error fetching username");
+        }
+        }
     const handleHover = () => {
         setOnHover(true);
     };
@@ -16,6 +87,12 @@ function Profile(){
         settoEdit(true);
         console.log('clicked ?', toEdit);
     };
+
+    useEffect(() => {
+        handleUsernameCheck();
+        handleDescriptionFetch();
+      });
+
     return(
         <div className="Profile">
             <div className="Profile-background"></div>
@@ -37,7 +114,7 @@ function Profile(){
                     <div className="Profile-info-square">
                         <div className="Profile-user-edit">
                             <div className="Profile-info-username">
-                                <h1>Username</h1>
+                                <h1>{username}</h1>
                             </div>
                             <button className="Profile-edit-button"placeholder="Edit Profile"
                                 onMouseEnter={handleHover}
@@ -49,7 +126,7 @@ function Profile(){
                             </button>
                         </div>
                         <div>
-                            <p className="Profile-info-description">Here to talk all things UF!</p>
+                            <p className="Profile-info-description">{descriptionText}</p>
                         </div>
                     </div>
                 </div>  
